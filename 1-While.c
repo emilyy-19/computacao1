77bc5be65861a89072b51493b2e630f43b8ef3bf
Emily Gonçalves <emilygoncalves@alunos.utfpr.edu.br>
2025-04-10 18:11:36 -0300

    1_ While

---
+++ 1-While.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+   int n, i=1;
+
+   printf("Digite um numero inteiro: ");
+   scanf("%d", &n);
+
+   while(i<=200){
+      if (i%2==0 && i%n==3){
+        printf("%d\n",i);
+      }
+      i=i+1;
+   }
+
+   return 0;
+}
