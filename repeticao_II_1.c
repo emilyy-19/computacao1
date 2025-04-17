c69878d31e63f20af871b515e092ea4bca6cd03c
Emily Gonçalves <emilygoncalves@alunos.utfpr.edu.br>
2025-04-17 17:03:41 -0300

    Add files via upload

--- repeticao_II_1.c
+++ repeticao_II_1.c
@@ -1,23 +1,25 @@
 #include<stdio.h>
-int main () {
-
-int num, maior = 0;
-
- printf("Digite um numero positivo (entrada), para sair digite 0 ou um numero negativo: ");
- scanf("%d", &num);
-
-
-while (num > 0)
+int main ()
 {
-    if(num > maior) {
-        maior = num;
+
+    int num, maior = 0;
+
+    printf("Digite um numero positivo (entrada), para sair digite 0 ou um numero negativo: ");
+    scanf("%d", &num);
+
+
+    while (num > 0)
+    {
+        if(num > maior)
+        {
+            maior = num;
+        }
+        printf ("digite outro numero, ou 0 para sair: ", num);
+
+        scanf ("%d", &num);
     }
-    printf ("digite outro numero, ou 0 para sair: ", num);
+    printf("Maior numero digitado : %d\n", maior);
 
-    scanf ("%d", &num);
-}
-printf("Maior numero digitado : %d\n", maior);
-
-return 0;
+    return 0;
 }
 
