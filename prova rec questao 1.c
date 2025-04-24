785a9f16434d915677ccfd36faff09bc9c139bbf
Emily Gonçalves <emilygoncalves@alunos.utfpr.edu.br>
2025-04-24 16:08:37 -0300

    Add files via upload

--- prova rec questao 1.c
+++ prova rec questao 1.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
- int main (){
+int main ()
+{
 
- float largura, altura;
- float area;
- int litros_tinta;
+    float largura, altura;
+    float area;
+    int litros_tinta;
 
- printf ("Digite a altura da parede (em metros): ");
- scanf ("%f", &altura);
- printf ("Digite a largura da parede (em metros): ");
- scanf ("%f", &largura);
+    printf ("Digite a altura da parede (em metros): ");
+    scanf ("%f", &altura);
+    printf ("Digite a largura da parede (em metros): ");
+    scanf ("%f", &largura);
 
- area = (altura * largura);
- litros_tinta = ceil (area/2);
+    area = (altura * largura);
+    litros_tinta = ceil (area/2);
 
- printf ("\n A area da parede eh: %.2f\n", area);
- printf ("A quantidade de litros de tinta necessaria para pintar a parede eh: %d", litros_tinta);
+    printf ("A area da parede eh: %.2f\n", area);
+    printf ("A quantidade de litros de tinta necessaria para pintar a parede eh: %d", litros_tinta);
 
-return 0;
- }
+    return 0;
+}
